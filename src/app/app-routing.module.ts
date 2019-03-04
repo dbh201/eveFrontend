bc55a6ede23b4bad26b58a87ef23bcaf0639e4eb
Daniel <db_win10_login@outlook.com>
2019-03-04 15:29:18 +0000

    debug outlines for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IgnoredListComponent } from './ignored-list/ignored-list.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { TradeCalculatorComponent } from './trade-calculator/trade-calculator.component';
+import { BuySellDepthComponent } from './buy-sell-depth/buy-sell-depth.component';
+import { PriceHistoryComponent } from './price-history/price-history.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'ignored-list',
+    component: IgnoredListComponent
+  }, {
+    path: 'item-details',
+    component: ItemDetailsComponent
+  }, {
+    path: 'trade-calculator',
+    component: TradeCalculatorComponent
+  }, {
+    path: 'buy-sell-depth',
+    component: BuySellDepthComponent
+  }, {
+    path: 'price-history',
+    component: PriceHistoryComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { IgnoredListComponent } from './ignored-list/ignored-list.component';
+import { ItemDetailsComponent } from './item-details/item-details.component';
+import { TradeCalculatorComponent } from './trade-calculator/trade-calculator.component';
+import { BuySellDepthComponent } from './buy-sell-depth/buy-sell-depth.component';
+import { PriceHistoryComponent } from './price-history/price-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    TopbarComponent,
+    SidebarComponent,
+    IgnoredListComponent,
+    ItemDetailsComponent,
+    TradeCalculatorComponent,
+    BuySellDepthComponent,
+    PriceHistoryComponent
   ],
   imports: [
     BrowserModule,
