0b93a2a81e33ae93c29135da8cbc593454790571
Daniel <db_win10_login@outlook.com>
2019-03-21 15:51:17 +0000

    named item properties instead of columns array

---
+++ src/app/item.ts
@@ -0,0 +1,17 @@
+export class Item {
+  typeID: number;
+  groupID: number;
+  typeName: string;
+  description: string;
+  mass: number;
+  volume: number;
+  capacity: number;
+  portionSize: number;
+  raceID: number;
+  basePrice: number;
+  published: number;
+  marketGroupID: number;
+  iconID: number;
+  soundID: number;
+  graphicID: number;
+}
