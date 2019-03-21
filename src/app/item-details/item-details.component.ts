cb192a2ec885d37e42848226b37805cfd454c82b
Daniel <db_win10_login@outlook.com>
2019-03-21 15:52:33 +0000

    item details now retrieved by the static-data service

--- src/app/item-details/item-details.component.ts
+++ src/app/item-details/item-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { of } from 'rxjs';
+import { Item } from '../item';
+import { StaticDataService } from '../static-data.service';
 
 @Component({
   selector: 'app-item-details',
@@ -8,41 +8,18 @@ import { of } from 'rxjs';
   styleUrls: ['./item-details.component.scss']
 })
 export class ItemDetailsComponent implements OnInit {
+  // TODO: 3d graphics ?
+  item: Item ;
+  icon = '' ;
   itemID: number ;
-  resultCols: Array<string> ;
-  result: Array<any> ;
-  details: { [key: string]: string } ;
-  constructor(private http: HttpClient) { }
+  constructor(private sds: StaticDataService) { }
 
   ngOnInit() {
-    this.resultCols = [
-      'typeID',
-      'groupID',
-      'typeName',
-      'description',
-      'mass',
-      'volume',
-      'capacity',
-      'portionSize',
-      'raceID',
-      'basePrice',
-      'published',
-      'marketGroupID',
-      'iconID',
-      'soundID',
-      'graphicID'
-    ];
-    this.details = { };
   }
   update() {
-    this.http.get<any>('http://localhost:5000/api/item/details/byitemid/' + this.itemID).subscribe(
-      r => {
-        this.result = r;
-        for ( let i = 0; i < this.resultCols.length ; i++ ) {
-          this.details[this.resultCols[i]] = this.result[0][i];
-        }
-        console.log(Object.keys(this.details));
-      }
-    );
+    this.sds.getItemDetails(this.itemID).subscribe(i => {
+      this.item = i;
+      this.sds.getIconURL(this.item.iconID).subscribe(j => {this.icon = j; console.log(this.icon + ' loaded.'); });
+    });
   }
 }
