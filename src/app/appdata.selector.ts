f6725e767f9404c4458ad74969eb1b0fe7367c30
Daniel <db_win10_login@outlook.com>
2019-04-01 21:26:19 +0000

    priceDataSelector

--- src/app/appdata.selector.ts
+++ src/app/appdata.selector.ts
@@ -1,6 +1,7 @@
 import { State } from './appdata.state';
 import { Observable } from 'rxjs';
 import { Item } from './item';
+import { PriceData } from './pricedata';
 export function typeIDSelector(state: any): number {
   state = state.state as State;
   return state.typeID;
@@ -8,4 +9,8 @@ export function typeIDSelector(state: any): number {
 export function itemSelector(state: any): Item {
   state = state.state as State;
   return state.item;
+}
+export function priceDataSelector(state: any): PriceData[] {
+  state = state.state as State;
+  return state.priceData;
 }
