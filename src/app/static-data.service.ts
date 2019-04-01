336c113e96e731b8b41f6e863dbe01dc127eee18
Daniel <db_win10_login@outlook.com>
2019-04-01 17:59:43 +0000

    icon url now in item class

--- src/app/static-data.service.ts
+++ src/app/static-data.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Store, select } from '@ngrx/store';
+import { State } from './appdata.state';
 import { Item } from './item';
 import { Observable } from 'rxjs';
+import { typeIDSelector } from './appdata.selector';
 import { map } from 'rxjs/operators';
+import * as a from './appdata.actions';
+
 const ICON_URL = 'http://localhost/Icons/items/';
 const STATIC_URL = 'http://localhost:5000/api/';
 
@@ -11,8 +16,7 @@ const STATIC_URL = 'http://localhost:5000/api/';
 })
 
 export class StaticDataService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getItemDetails(itemID: number): Observable<Item> {
     return this.http.get(STATIC_URL + 'item/details/byitemid/' + itemID)
@@ -33,7 +37,8 @@ export class StaticDataService {
               marketGroupID: i[0][11],
               iconID: i[0][12],
               soundID: i[0][13],
-              graphicID: i[0][14]
+              graphicID: i[0][14],
+              iconURL: ''
             } as Item;
           return r;
         }));
