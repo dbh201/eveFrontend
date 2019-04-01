a309c84ad7342dce929c3c5c626f44a7711d7a98
Daniel <db_win10_login@outlook.com>
2019-04-01 21:25:15 +0000

    added market data actions

--- src/app/appdata.actions.ts
+++ src/app/appdata.actions.ts
@@ -2,12 +2,19 @@ import { Action } from '@ngrx/store';
 import { Item } from './item';
 
 export enum ActionType {
-  Item = '[Global] Item Action',
+  Item    = '[Global] Item Action',
+  Market  = '[Global] Market Data Action',
+}
+export enum MarketActions {
+  RequestPriceHistory = '[Market] Request price history for globally selected item',
+  AddMarketRegion = '[Market] Add region to relevant market regions list',
+  RemoveMarketRegion = '[Market] Remove region from relevant market regions list',
+  ClearMarketRegions = '[Market] Remove all regions from relevant market regions list',
 }
 
 export enum ItemActions {
-  SetItem = '[Global] Set selected item',
-  SyncItem = '[Global] Sync item details',
+  SetItem   = '[Item] Set globally selected item',
+  SyncItem  = '[Item] Sync item details in global ngrx store',
 }
 export class SetItem implements Action {
   readonly type = ActionType.Item ;
@@ -19,7 +26,30 @@ export class SyncItem implements Action {
   readonly action = ItemActions.SyncItem ;
   constructor(public item: Item) {}
 }
-export type ActionsUnion = ItemActionsUnion;
-export type ActionTypesUnion = Item;
+
+
+export class RequestPriceHistory implements Action {
+  readonly type = ActionType.Market ;
+  readonly action = MarketActions.RequestPriceHistory ;
+}
+export class AddMarketRegion implements Action {
+  readonly type = ActionType.Market ;
+  readonly action = MarketActions.AddMarketRegion ;
+  constructor(public regionID: number) {}
+}
+export class RemoveMarketRegion implements Action {
+  readonly type = ActionType.Market ;
+  readonly action = MarketActions.RemoveMarketRegion ;
+  constructor(public regionID: number) {}
+}
+export class ClearMarketRegions implements Action {
+  readonly type = ActionType.Market ;
+  readonly action = MarketActions.ClearMarketRegions ;
+}
+
+
+export type ActionsUnion = ItemActionsUnion | MarketActionsUnion;
 
 export type ItemActionsUnion = SetItem | SyncItem;
+export type MarketActionsUnion = RequestPriceHistory |
+  AddMarketRegion | RemoveMarketRegion | ClearMarketRegions;
