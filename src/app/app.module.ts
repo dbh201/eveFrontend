5d3ae361e9b369c067a97ae593a0570929a02a06
Daniel <db_win10_login@outlook.com>
2019-04-01 17:58:25 +0000

    added store module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { rootReducer } from './appdata.reducer';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +16,7 @@ import { ItemDetailsComponent } from './item-details/item-details.component';
 import { TradeCalculatorComponent } from './trade-calculator/trade-calculator.component';
 import { BuySellDepthComponent } from './buy-sell-depth/buy-sell-depth.component';
 import { PriceHistoryComponent } from './price-history/price-history.component';
+import { StaticDataService } from './static-data.service';
 
 @NgModule({
   declarations: [
@@ -31,9 +34,10 @@ import { PriceHistoryComponent } from './price-history/price-history.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({ state: rootReducer })
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
