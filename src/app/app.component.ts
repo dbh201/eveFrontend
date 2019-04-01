819082b74d3e44005a81da1ed0f7c3863fabda4a
Daniel <db_win10_login@outlook.com>
2019-04-01 20:15:42 +0000

    removed debug code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,26 +14,20 @@ import { Subscription } from 'rxjs';
 export class AppComponent {
   title = 'eveFrontend';
   typeID$: Subscription;
-  state: string;
   constructor(private sds: StaticDataService, private store: Store<State>) {}
   ngOnInit() {
     console.log('App loading background service...');
     this.typeID$ = this.store.select(typeIDSelector).subscribe( {
       next: (tid) => {
-        console.log( 'app found tid ' + tid );
-        if ( tid >= 0 ) {
-          console.log('Data service received tid: ' + tid);
+        if ( tid > 0 ) {
           this.sds.getItemDetails(tid).subscribe( j => {
-            console.log('Data service received item: ' + j.typeID);
 
             this.sds.getIconURL(j.iconID).subscribe( k => {
-              console.log('Data service received icon: ' + k);
               j.iconURL = k;
               this.store.dispatch( new a.SyncItem(j) );
             });
           });
         }
       }});
-    this.store.subscribe(b => { this.state = JSON.stringify(b); });
   }
 }
