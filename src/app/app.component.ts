7e58d9aa2e0171d9102b9d1ca12170e676600352
Daniel <db_win10_login@outlook.com>
2019-04-01 17:57:57 +0000

    background service for selecting an item

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { StaticDataService } from './static-data.service';
+import { Store, select } from '@ngrx/store';
+import { State } from './appdata.state';
+import { typeIDSelector } from './appdata.selector';
+import * as a from './appdata.actions';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eveFrontend';
+  typeID$: Subscription;
+  state: string;
+  constructor(private sds: StaticDataService, private store: Store<State>) {}
+  ngOnInit() {
+    console.log('App loading background service...');
+    this.typeID$ = this.store.select(typeIDSelector).subscribe( {
+      next: (tid) => {
+        console.log( 'app found tid ' + tid );
+        if ( tid >= 0 ) {
+          console.log('Data service received tid: ' + tid);
+          this.sds.getItemDetails(tid).subscribe( j => {
+            console.log('Data service received item: ' + j.typeID);
+
+            this.sds.getIconURL(j.iconID).subscribe( k => {
+              console.log('Data service received icon: ' + k);
+              j.iconURL = k;
+              this.store.dispatch( new a.SyncItem(j) );
+            });
+          });
+        }
+      }});
+    this.store.subscribe(b => { this.state = JSON.stringify(b); });
+  }
 }
--- src/app/appdata.selector.ts
+++ src/app/appdata.selector.ts
@@ -1,9 +1,10 @@
-import { State } from './appdata.reducer';
+import { State } from './appdata.state';
 import { Observable } from 'rxjs';
 import { Item } from './item';
 export function typeIDSelector(state: State): number {
   console.log(JSON.stringify(state));
-  return state.state.typeID;
+  console.log(state.typeID);
+  return state.typeID;
 }
 export function itemSelector(state: State): Item {
   return state.item;
