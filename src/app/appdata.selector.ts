58204f2e235050fe9e778132c8051ae005166ff5
Daniel <db_win10_login@outlook.com>
2019-04-01 17:59:18 +0000

    selectors now work without error messages

--- src/app/appdata.selector.ts
+++ src/app/appdata.selector.ts
@@ -1,11 +1,11 @@
 import { State } from './appdata.state';
 import { Observable } from 'rxjs';
 import { Item } from './item';
-export function typeIDSelector(state: State): number {
-  console.log(JSON.stringify(state));
-  console.log(state.typeID);
+export function typeIDSelector(state: any): number {
+  state = state.state as State;
   return state.typeID;
 }
-export function itemSelector(state: State): Item {
+export function itemSelector(state: any): Item {
+  state = state.state as State;
   return state.item;
 }
