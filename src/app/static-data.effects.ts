d505e5c4727c497abdab64f74a0903dcfd75a2e0
Daniel <db_win10_login@outlook.com>
2019-04-05 00:31:15 +0000

    ngrx effects module for static-data

---
+++ src/app/static-data.effects.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Observable } from 'rxjs';
+import { switchMap, withLatestFrom, map, tap } from 'rxjs/operators';
+import { StaticDataService } from './static-data.service';
+import { ItemActions, RequestItemDetails, RequestIconURLForItem, SyncItem } from './appdata.actions';
+import { Item } from './item';
+
+@Injectable()
+
+export class StaticDataEffects {
+
+  constructor(
+    private sds: StaticDataService,
+    private actions$: Actions ) {}
+
+  @Effect()
+  itemDetailsEffect$ = this.actions$.pipe(
+    ofType( ItemActions.RequestItemDetails ),
+    switchMap( (a: RequestItemDetails) => this.sds.getItemDetails(a.typeID)
+      .pipe(
+        map( (b: Item) => new RequestIconURLForItem(b) )
+      )
+    )
+  );
+
+  @Effect()
+  itemIconEffect$ = this.actions$.pipe(
+    ofType( ItemActions.RequestIconURLForItem ),
+    switchMap( (a: RequestIconURLForItem) => this.sds.populateIconURLProperty(a.item)
+      .pipe(
+        map( (b: Item) => new SyncItem(b) )
+      )
+    )
+  );
+}
