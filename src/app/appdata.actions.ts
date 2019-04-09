729bb64d70702f359a6b95ced3dc2d92f19b1f38
Daniel <db_win10_login@outlook.com>
2019-04-09 16:28:10 +0000

    added AddPriceHistoryForRegion and RequestPriceHistoryForRegion actions

--- src/app/appdata.actions.ts
+++ src/app/appdata.actions.ts
@@ -1,23 +1,31 @@
 import { Action } from '@ngrx/store';
 import { Item } from './item';
+import { PriceHistoryEntry } from './price-history';
+
+
 
 export enum ActionCategories {
   Item    = '[Global] Item Action',
   Market  = '[Global] Market Data Action',
 }
+
 export enum MarketActions {
-  RequestPriceHistory = '[Market] Request price history for globally selected item',
-  AddMarketRegion = '[Market] Add region to relevant market regions list',
-  RemoveMarketRegion = '[Market] Remove region from relevant market regions list',
-  ClearMarketRegions = '[Market] Remove all regions from relevant market regions list',
+  RequestPriceHistory           = '[Market] Request price history for globally selected item',
+  RequestPriceHistoryForRegion  = '[Market] Request price history for globally selected item in a specific region',
+  AddPriceHistoryForRegion      = '[Market] Add price history for region to ngrx store',
+  AddMarketRegion               = '[Market] Add region to relevant market regions list',
+  RemoveMarketRegion            = '[Market] Remove region from relevant market regions list',
+  ClearMarketRegions            = '[Market] Remove all regions from relevant market regions list',
 }
 
 export enum ItemActions {
-  SetItem   = '[Item] Set globally selected item',
-  SyncItem  = '[Item] Sync item details in global ngrx store',
-  RequestItemDetails = '[Item] Request item details from external API',
-  RequestIconURLForItem = '[Item] Request icon URL for an item from external API',
+  SetItem                       = '[Item] Set globally selected item',
+  SyncItem                      = '[Item] Sync item details in global ngrx store',
+  RequestItemDetails            = '[Item] Request item details from external API',
+  RequestIconURLForItem         = '[Item] Request icon URL for an item from external API',
 }
+
+
 
 export class RequestItemDetails implements Action {
   readonly category = ActionCategories.Item ;
@@ -36,6 +44,7 @@ export class SetItem implements Action {
   readonly type = ItemActions.SetItem ;
   constructor(public typeID: number) {}
 }
+
 export class SyncItem implements Action {
   readonly category = ActionCategories.Item ;
   readonly type = ItemActions.SyncItem ;
@@ -43,28 +52,48 @@ export class SyncItem implements Action {
 }
 
 
+
 export class RequestPriceHistory implements Action {
   readonly category = ActionCategories.Market ;
   readonly type = MarketActions.RequestPriceHistory ;
+  constructor(public typeID: number) {}
 }
+
+export class RequestPriceHistoryForRegion implements Action {
+  readonly category = ActionCategories.Market ;
+  readonly type = MarketActions.RequestPriceHistoryForRegion ;
+  constructor(public typeID: number, public regionID: number) {}
+}
+
+export class AddPriceHistoryForRegion implements Action {
+  readonly category = ActionCategories.Market ;
+  readonly type = MarketActions.AddPriceHistoryForRegion ;
+  constructor(public priceHistory: PriceHistoryEntry[], public regionID: number) {}
+}
+
 export class AddMarketRegion implements Action {
   readonly category = ActionCategories.Market ;
   readonly type = MarketActions.AddMarketRegion ;
   constructor(public regionID: number) {}
 }
+
 export class RemoveMarketRegion implements Action {
   readonly category = ActionCategories.Market ;
   readonly type = MarketActions.RemoveMarketRegion ;
   constructor(public regionID: number) {}
 }
+
 export class ClearMarketRegions implements Action {
   readonly category = ActionCategories.Market ;
   readonly type = MarketActions.ClearMarketRegions ;
 }
 
 
+
 export type ActionsUnion = ItemActionsUnion | MarketActionsUnion;
 
 export type ItemActionsUnion = SetItem | SyncItem;
-export type MarketActionsUnion = RequestPriceHistory |
+
+export type MarketActionsUnion = RequestPriceHistory | RequestPriceHistoryForRegion |
+  AddPriceHistoryForRegion |
   AddMarketRegion | RemoveMarketRegion | ClearMarketRegions;
