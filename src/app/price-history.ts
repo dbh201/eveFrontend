92df79022ffdd78c235b53c194bbabcff5df59c5
Daniel <db_win10_login@outlook.com>
2019-04-09 16:15:31 +0000

    renamed pricedata.ts to price-history.ts

---
+++ src/app/price-history.ts
@@ -0,0 +1,22 @@
+export class PriceHistoryEntry {
+  average: number;
+  date: string;
+  highest: number;
+  lowest: number;
+  orderCount: number;
+  volume: number;
+}
+// FIXME: Map is only available in ES6, but target is ES5, and switching the target to ES6
+// in tsconfig.json doesn't do anything. I still want to use Map<number, PriceHistoryEntry[]>
+// but I guess I can't yet. Calling data.set() in any reducer does nothing, even with typecasting.
+//
+//
+// export class PriceHistory {
+//   data: Map<number, PriceHistoryEntry[]>;
+//   constructor() { this.data = new Map<number, PriceHistoryEntry[]>(); }
+// }
+
+export class PriceHistory {
+  data: { [key: number]: PriceHistoryEntry[] };
+  constructor() { this.data = {}; }
+}
--- src/app/pricedata.ts
+++
@@ -1,8 +0,0 @@
-export class PriceData {
-  average: number;
-  date: string;
-  highest: number;
-  lowest: number;
-  orderCount: number;
-  volume: number;
-}
