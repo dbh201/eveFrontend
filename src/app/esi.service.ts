323e89be325e3d141f71b3b2bd4e0ff552183ed8
Daniel <db_win10_login@outlook.com>
2019-04-09 16:26:43 +0000

    added getPriceHistoryForRegion and a simple error handler

--- src/app/esi.service.ts
+++ src/app/esi.service.ts
@@ -1,34 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Actions, Effect, ofType } from '@ngrx/effects';
-import { MarketActions } from './appdata.actions';
-import { PriceData } from './pricedata';
+import { Observable, from, merge, empty } from 'rxjs';
+import { map, reduce, switchMap, tap, catchError } from 'rxjs/operators';
+import { PriceHistoryEntry } from './price-history';
 
-const ESI_URL = 'https://esi.evetech.net/latest/';
+const ESI_URL = 'http://esi.local/latest/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ESIService {
 
-  constructor(private http: HttpClient, private actions$: Actions) { }
+  constructor(private http: HttpClient) { }
 
-  getPriceHistory(typeID: number, regionID: number): Observable<PriceData[]> {
-    return this.http.get(ESI_URL + 'markets/' + regionID + 'history/?type_id=' + typeID)
-      .pipe(
-        map( (i) => {
-          // 'a is never redefined blah blah blah', whatever. here.
-          let a = null;
-          a = new Array<PriceData>();
-          for (const j of Array<any>(i)) {
-            a.push( {
-              ...j, orderCount:  j.order_count
-            } as PriceData);
-          }
-          return a;
-        })
-      );
+  getPriceHistoryForRegion(typeID: number, regionID: number): Observable<PriceHistoryEntry[]> {
+    return this.http.get(ESI_URL + 'markets/' + regionID + '/history/?type_id=' + typeID)
+    .pipe(
+      switchMap( i => Object.values(i) ),
+      catchError( (error) => this.handleError(error)),
+      reduce( (j: PriceHistoryEntry[], k: any) => {
+        j.push({ ...k, orderCount: k.order_count} as PriceHistoryEntry);
+        return j;
+      }, (new Array<PriceHistoryEntry>()))
+    );
+  }
+  handleError(error) {
+    console.log('ESI had an error:\n' + error);
+    return empty();
   }
 }
