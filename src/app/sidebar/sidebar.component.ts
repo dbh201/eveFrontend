58b919f429b4f7b123b02ed8c92b200f8da3ed2d
Daniel <db_win10_login@outlook.com>
2019-04-09 16:32:00 +0000

    this component is still in a debug state - added setItem functionality for app state,removed some state info from html

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { State } from '../appdata.state';
+import { SetItem } from '../appdata.actions';
 import { Observable } from 'rxjs';
 
 
@@ -11,10 +12,14 @@ import { Observable } from 'rxjs';
 })
 export class SidebarComponent implements OnInit {
   propBuffer: any;
+  typeID: number;
   constructor(private store: Store<State>) { }
   state$: Observable<State>;
   ngOnInit() {
     console.log('Sidebar loaded with defaults.');
     this.state$ = this.store.select('state');
+  }
+  setItem() {
+    this.store.dispatch(new SetItem(this.typeID));
   }
 }
