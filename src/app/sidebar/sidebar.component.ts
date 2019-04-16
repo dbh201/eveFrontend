687c5f344874d7770238670d8d223c8383385a8e
Daniel <db_win10_login@outlook.com>
2019-04-16 17:54:35 +0000

    DEBUG: added direct firing of add/remove regionlist ... what about side effects?

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { State } from '../root.state';
-import { SetItem } from '../root.actions';
+import { SetItem, AddMarketRegion, RemoveMarketRegion } from '../root.actions';
 import { Observable } from 'rxjs';
 
 
@@ -13,11 +13,18 @@ import { Observable } from 'rxjs';
 export class SidebarComponent implements OnInit {
   propBuffer: any;
   typeID: number;
+  regionID: number;
   constructor(private store: Store<State>) { }
   state$: Observable<State>;
   ngOnInit() {
     console.log('Sidebar loaded with defaults.');
     this.state$ = this.store.select('state');
+  }
+  addRegion() {
+    this.store.dispatch(new AddMarketRegion(this.regionID));
+  }
+  rmRegion() {
+    this.store.dispatch(new RemoveMarketRegion(this.regionID));
   }
   setItem() {
     this.store.dispatch(new SetItem(this.typeID));
