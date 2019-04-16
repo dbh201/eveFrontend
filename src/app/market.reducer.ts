6afc60e234ae4a86ac98b6aabb7c9694ae77835f
Daniel <db_win10_login@outlook.com>
2019-04-16 17:53:33 +0000

    added support for regionlist add/remove

--- src/app/market.reducer.ts
+++ src/app/market.reducer.ts
@@ -10,8 +10,26 @@ export function marketReducer(
     case a.MarketActions.AddPriceHistoryForRegion: {
       const b = { ...state };
       b.priceHistory = new PriceHistory();
+
+      if ( state.priceHistory.typeID === action.typeID ) {
+        b.priceHistory.data = Object.assign({}, state.priceHistory.data);
+      }
       b.priceHistory.data[action.regionID] = action.priceHistory;
-      console.log('MARKET: new state:\n' + JSON.stringify(b));
+      return b;
+    }
+    case a.MarketActions.AddMarketRegion: {
+      const b = { ...state };
+      b.regionList = Object.assign([], state.regionList);
+      b.regionList.push(action.regionID);
+      return b;
+    }
+    case a.MarketActions.RemoveMarketRegion: {
+      const b = { ...state };
+      b.regionList = Object.assign([], state.regionList).filter( r => r !== action.regionID );
+      const i = b.regionList.indexOf(action.regionID);
+      if ( i > -1) {
+        b.regionList.splice(i, 1);
+      }
       return b;
     }
     default: {
